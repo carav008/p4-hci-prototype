3ebe63e7a0041449679e2189858bdb025d37d9fe
bushw011 <bushw011@morris.umn.edu>
2018-04-25 14:06:17 -0500

    Set up connections component and declare within app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ConnectionsComponent} from "./connections-component/connections.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule,
